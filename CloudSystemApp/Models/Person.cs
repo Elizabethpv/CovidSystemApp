2bf674cceeef6862a64d9add63a6c6e605476fe6
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-06-06 18:50:57 +0530

    Add project files.

---
+++ CloudSystemApp/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudSystemApp.Models
+{
+    public class Person
+    {
+
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String Email { get; set; }
+        public String AdharNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String Password { get; set; }
+
+        
+}
